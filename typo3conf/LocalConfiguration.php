20129fc7d2d4e2b6e3f4fc6ba63e0cba2b96d2c9
SvenJ84 <svenjaeger@Svens-MacBook-Pro.local>
2014-10-13 14:58:39 +0200

    Initialisierung

---
+++ typo3conf/LocalConfiguration.php
@@ -0,0 +1,68 @@
+<?php
+return array(
+	'BE' => array(
+		'debug' => FALSE,
+		'explicitADmode' => 'explicitAllow',
+		'installToolPassword' => '$P$C8.al9WGpdvKsOFx4MznIWSY8UxGAs/',
+		'loginSecurityLevel' => 'rsa',
+	),
+	'DB' => array(
+		'database' => 'typo3_sven',
+		'extTablesDefinitionScript' => 'extTables.php',
+		'host' => 'localhost',
+		'password' => 'ibilgij1984!',
+		'port' => 3306,
+		'username' => 'root',
+	),
+	'EXT' => array(
+		'extConf' => array(
+			'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+			'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+		),
+	),
+	'FE' => array(
+		'activateContentAdapter' => FALSE,
+		'debug' => FALSE,
+		'loginSecurityLevel' => 'rsa',
+	),
+	'GFX' => array(
+		'colorspace' => 'sRGB',
+		'im' => 1,
+		'im_mask_temp_ext_gif' => 1,
+		'im_path' => '/usr/local/bin/',
+		'im_path_lzw' => '/usr/local/bin/',
+		'im_v5effects' => 1,
+		'im_version_5' => 'im6',
+		'image_processing' => 1,
+		'jpg_quality' => '80',
+	),
+	'SYS' => array(
+		'caching' => array(
+			'cacheConfigurations' => array(
+				'extbase_object' => array(
+					'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+					'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+					'groups' => array(
+						'system',
+					),
+					'options' => array(
+						'defaultLifetime' => 0,
+					),
+				),
+			),
+		),
+		'clearCacheSystem' => FALSE,
+		'compat_version' => '6.2',
+		'devIPmask' => '',
+		'displayErrors' => 0,
+		'enableDeprecationLog' => FALSE,
+		'encryptionKey' => 'd4db0400ad977b9b8ef954edead91575d73aed64f0baddd331a3f474019cb2918f674aefa627ccf2a93cf4b807f4a837',
+		'isInitialInstallationInProgress' => FALSE,
+		'sitename' => 'Sven Jäger',
+		'sqlDebug' => 0,
+		'systemLogLevel' => 2,
+		't3lib_cs_convMethod' => 'mbstring',
+		't3lib_cs_utils' => 'mbstring',
+	),
+);
+?>
